6ba3fe2f58cdc612af373e857139531653f24af0
Bruno <bruno@awdd.com.br>
2025-07-08 13:59:09 -0300

    feat: improve start function by using ClusterBuilder

--- src/builder.rs
+++ src/builder.rs
@@ -7,7 +7,7 @@ use tokio::sync::oneshot;
 use tonic::transport::Server;
 
 use crate::{
-    cluster::{cluster::Cluster, node::Node, service::ClusterService}, env::{config::EnvConfig, consensus::ConsensusEngine, node::Graph, storage::Storage, AtlasEnv}, network::{
+    cluster::{cluster::Cluster, builder::ClusterBuilder, node::Node, service::ClusterService}, env::{config::EnvConfig, consensus::ConsensusEngine, node::Graph, storage::Storage, AtlasEnv}, network::{
         adapter::NetworkAdapter,
         grcp_adapter::GRPCNetworkAdapter, 
     }, peer_manager::{self, PeerManager}, utils::NodeId
@@ -20,12 +20,19 @@ pub fn init(network: Arc<RwLock<dyn NetworkAdapter>>, path: Option<&str>) {
     create_env(network, peer_manager, path);
 }
 
-pub fn start(network: Arc<RwLock<dyn NetworkAdapter>>, path: Option<&str>) {
+pub async fn start(network: Arc<RwLock<dyn NetworkAdapter>>, path: Option<&str>, id: String) -> Result<Cluster, Box<dyn std::error::Error>> {
     let env = build_env(Arc::clone(&network), path);
-    let node_id = NodeId("Node".to_string());
-    Cluster::new(env, Arc::clone(&network), node_id);
-}
+    let node_id = NodeId(id);
+    let cluster = ClusterBuilder::new()
+        .with_env(env)
+        .with_network(Arc::clone(&network))
+        .with_node_id(node_id)
+        .start_with_grpc()
+        .await
+        .map_err(|e| e.to_string())?; 
 
+    Ok(cluster)
+}
 
 fn build_env(network: Arc<RwLock<dyn NetworkAdapter>>, path: Option<&str>) -> AtlasEnv {
     load_env(path.unwrap_or("config.json"), Arc::clone(&network))
@@ -49,3 +56,4 @@ fn load_env(path: &str, network: Arc<RwLock<dyn NetworkAdapter>>) -> AtlasEnv {
         .expect("Failed to load config file")
         .build_env(network)
 }
+
