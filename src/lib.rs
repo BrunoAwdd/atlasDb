042f98ac8b4d303d03d38300690c2a6e03ddb464
Bruno <bruno@awdd.com.br>
2025-07-08 13:31:15 -0300

    chore: reorder imports alphabetically and improve public API display in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,39 @@
 // lib.rs
-
-pub mod cluster;
-//pub mod ffi;
-pub mod network;
-pub mod utils;
-pub mod peer_manager;
-pub mod env;
 pub mod builder;
+pub mod cluster;
 pub mod cluster_proto {
     tonic::include_proto!("cluster");
 }
+pub mod env;
+//pub mod ffi;
+pub mod network;
+pub mod peer_manager;
+pub mod utils;
 
-
-// Reexporta os tipos principais para quem usar a lib
-
-pub use cluster::{cluster::Cluster, node::Node};
-pub use env::{
-    consensus::{ConsensusEngine, Proposal, Vote, ConsensusResult},
-    node::{Graph, Vertex, Edge},
-    storage::{Storage, audit::{AuditData, save_audit, load_audit}}
+pub use cluster::{
+    cluster::Cluster, 
+    node::Node
 };
-
+pub use env::{
+    consensus::{
+        ConsensusEngine,
+        ConsensusResult, 
+        Vote
+    },
+    node::{
+        Edge,
+        Graph, 
+        Vertex, 
+    },
+    proposal::Proposal,
+    storage::{
+        Storage, 
+        audit::{
+            AuditData,
+            load_audit, 
+            save_audit
+        }
+    }
+};
 pub use utils::NodeId;
-
-pub use crate::network::adapter::NetworkAdapter;
+pub use network::adapter::NetworkAdapter;
