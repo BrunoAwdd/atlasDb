0c8c843dc2d964790848b5124fca7e80368e0e33
Bruno <bruno@awdd.com.br>
2025-07-19 18:05:19 -0300

    feat: implement VoteBatch and ProposalBatch messages and refactor constructors

--- src/cluster/service.rs
+++ src/cluster/service.rs
@@ -7,7 +7,9 @@ use crate::cluster_proto::{
     Ack, 
     HeartbeatMessage, 
     ProposalMessage, 
-    VoteMessage
+    VoteMessage,
+    VoteBatch,
+    ProposalBatch
 };
 
 pub struct ClusterService {
@@ -15,8 +17,9 @@ pub struct ClusterService {
 }
 
 impl ClusterService {
-    pub fn new(cluster: Arc<tokio::sync::RwLock<crate::cluster::cluster::Cluster>>) -> Self {
-        println!("Created");
+    pub fn new(
+        cluster: Arc<tokio::sync::RwLock<crate::cluster::cluster::Cluster>>
+    ) -> Self {
         ClusterService { cluster }
     }
 }
@@ -28,7 +31,25 @@ impl ClusterNetwork for ClusterService {
         request: Request<HeartbeatMessage>,
     ) -> Result<Response<Ack>, Status> {
         println!("Received heartbeat from: {}", request.get_ref().from);
-        let ack = self.cluster.read().await.handle_heartbeat(request.into_inner()).await;
+        let ack = self.cluster.read().await.handle_heartbeat(request.into_inner());
+        Ok(Response::new(ack))
+    }
+
+    async fn submit_vote_batch(
+        &self,
+        request: Request<VoteBatch>,
+    ) -> Result<Response<Ack>, Status> {
+        println!("Received vote batch from: {}", request.get_ref().votes[0].voter_id);
+        let ack = self.cluster.write().await.handle_vote_batch(request.into_inner()).map_err(|e| Status::internal(format!("handle_vote_batch error: {}", e)))?;
+        Ok(Response::new(ack))
+    }
+
+    async fn submit_proposal_batch(
+        &self,
+        request: Request<ProposalBatch>,
+    ) -> Result<Response<Ack>, Status> {
+        println!("Received proposal batch from: {}", request.get_ref().proposals[0].proposer_id);
+        let ack = self.cluster.write().await.handle_proposal_batch(request.into_inner()).map_err(|e| Status::internal(format!("handle_proposal_batch error: {}", e)))?;
         Ok(Response::new(ack))
     }
 
@@ -37,7 +58,7 @@ impl ClusterNetwork for ClusterService {
         request: Request<ProposalMessage>,
     ) -> Result<Response<Ack>, Status> {
         println!("Received proposal from: {}", request.get_ref().proposer_id);
-        let ack = self.cluster.read().await.handle_proposal(request.into_inner()).await;
+        let ack = self.cluster.write().await.handle_proposal(request.into_inner()).map_err(|e| Status::internal(format!("handle_proposal error: {}", e)))?;
         Ok(Response::new(ack))
     }
 
@@ -45,8 +66,9 @@ impl ClusterNetwork for ClusterService {
         &self,
         request: Request<VoteMessage>,
     ) -> Result<Response<Ack>, Status> {
+        // TODO: Implement this
         //let ack = self.cluster.read().await.handle_vote(request.into_inner()).await;
         //Ok(Response::new(ack))
-        Ok( Response::new(Ack { received: true, message: "Not implmented".to_string() }))
+        Ok( Response::new(Ack { received: true, message: "Not implemented".to_string() }))
     }
 }
