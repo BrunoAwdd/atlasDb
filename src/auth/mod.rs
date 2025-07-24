e903b1f49485c13bab2d31bb8b1a4fa84302b5de
Bruno <bruno@awdd.com.br>
2025-07-24 16:24:13 -0300

    style: remove redundant parentheses from Result<bool, String> in Authenticator trait

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -2,5 +2,5 @@ pub mod authenticator;
 
 pub trait Authenticator: Send + Sync {
     fn sign(&self, message: Vec<u8>, password: String) -> Result<Vec<u8>, String>;
-    fn verify(&self, message: Vec<u8>, signature: Vec<u8>) -> Result<(bool), String>;
+    fn verify(&self, message: Vec<u8>, signature: Vec<u8>) -> Result<bool, String>;
 }
