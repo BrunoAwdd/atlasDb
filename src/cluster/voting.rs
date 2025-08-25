07108747a717f8772e3a1f425e9ae3a6e0f7201e
Bruno <bruno@awdd.com.br>
2025-08-25 15:25:02 -0300

    :sparkles: feat(voting): implement automated proposal voting and signing

--- src/cluster/voting.rs
+++ src/cluster/voting.rs
@@ -4,22 +4,59 @@ use crate::{
     cluster::core::Cluster,
     cluster_proto::{Ack, VoteMessage},
     network::adapter::{ClusterMessage, VoteData},
-    utils::NodeId,
+    utils::NodeId, Vote,
 };
 
 impl Cluster {
-    pub async fn vote_proposals(&mut self, vote: ClusterMessage, proposer_id: NodeId) -> Result<(), String> {    
+    pub(super) async fn vote_proposals(&self) -> Result<(), String> {
+        let engine_guard = self.local_env.engine.lock().await;
+        let proposal_pool = engine_guard.get_all_proposals().all().clone();
+        drop(engine_guard);
+
+        for (_, proposal) in proposal_pool{
+            let content = proposal.content.clone();
+            let serialized = bincode::serialize(&content).unwrap();
+
+            let is_valid = self.auth.read().await.verify(serialized, &proposal.signature);
+
+            let vote = match is_valid {
+                Ok(true) => Vote::Yes,
+                Ok(false) => Vote::No,
+                Err(_e) => Vote::Abstain
+            };
+
+            let vote_to_sign = VoteData {        
+                proposal_id: proposal.id.clone(),
+                vote: vote,
+                voter: self.local_node.id.clone(),
+            };
+
+            let vote_serialized = bincode::serialize(&vote_to_sign).unwrap();
+            let signed_vote = self.auth.read().await.sign(vote_serialized.clone(), "12345".to_string())?;
+    
+            let vote_data = vote_to_sign.into_cluster_message(Vec::new(), signed_vote);
+
+            self.vote_proposal(vote_data, proposal.proposer).await?;
+
+            println!("Sending Votes...");
+        }
+
+
+        Ok(())
+    }
+
+    async fn vote_proposal(&self, vote: ClusterMessage, proposer_id: NodeId) -> Result<(), String> {    
         // Busca o proposer
         let proposer = self.peer_manager
             .read()
-            .map_err(|_| "Failed to lock peer manager")?
+            .await
             .get_peer_stats(&proposer_id)
             .ok_or_else(|| format!("Proposer node {} not found", proposer_id))?;
         
         self.local_env
-            .write()
-            .map_err(|_| "Failed to acquire write lock on local env")?
             .engine
+            .lock()
+            .await
             .vote_proposals(
                 vote,
                 Arc::clone(&self.network),
