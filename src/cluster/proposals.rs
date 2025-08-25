96abafff2cc28a00c305c970d8c2b55cb7914bc6
Bruno <bruno@awdd.com.br>
2025-08-25 14:56:20 -0300

    :sparkles: feat(proposals): add add_proposal and broadcast_proposal functions

--- src/cluster/proposals.rs
+++ src/cluster/proposals.rs
@@ -4,14 +4,51 @@ use crate::{
     cluster_proto::{
         Ack, 
         ProposalMessage
-    },
-    cluster::core::Cluster,
-    env::proposal::Proposal
+    }, 
+    cluster::core::Cluster, 
+    env::proposal::Proposal, 
+    NodeId
 };
 
 impl Cluster {
+    pub(super) async fn add_proposal(&self, proposal: Proposal) -> Result<(), String> {
+        self.local_env.engine.lock().await
+            .add_proposal(proposal.clone());
+
+        Ok(())
+    }
+
+    pub(super) async fn broadcast_proposals(&self) -> Result<(), String> {
+        let peers = {
+            let manager = self.peer_manager.read().await;
+            manager.get_active_peers().iter().cloned().collect::<Vec<NodeId>>()
+        };
+
+        let sender_id = self.local_node.id.clone();
+        let mut errors = Vec::new();
+        let proposals = self.local_env.engine.lock()
+            .await            
+            .pool
+            .all()
+            .clone();
+
+
+        // Ao inves de loop em proposal, vou enviar todas
+        for (_, proposal) in proposals {
+            for peer_id in &peers {
+                if peer_id != &sender_id {
+                    if let Err(e) = self.submit_proposal(proposal.clone()).await {
+                        errors.push(format!("Failed to submit proposal to {}: {}", peer_id, e));
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
     /// Sends a proposal to a specific peer
-    pub async fn submit_proposal(&self, proposal: Proposal) -> Result<Vec<Result<Ack, String>>, String> {
+    async fn submit_proposal(&self, proposal: Proposal) -> Result<Vec<Result<Ack, String>>, String> {
         let ack = self
             .local_env
             .engine
@@ -27,9 +64,9 @@ impl Cluster {
         Ok(ack)
     }
 
-    pub fn handle_proposal(&mut self, msg: ProposalMessage) -> Result<Ack, String> {
-        let proposal = Proposal::from_proto(msg).map_err(|e| format!("Failed to parse proposal: {}", e))?;
-    
+    pub(super) async fn handle_proposal(&self, msg: ProposalMessage) -> Result<Ack, String> {
+        let proposal = Proposal::from_proto(msg.clone()).map_err(|e| format!("Failed to parse proposal: {}", e))?;
+
         let bytes = match bincode::serialize(&proposal.content) {
             Ok(b) => b,
             Err(e) => {
@@ -40,18 +77,10 @@ impl Cluster {
                 });
             }
         };
-    
-        let auth = match self.auth.read() {
-            Ok(a) => a,
-            Err(_) => {
-                println!("⚠️ Failed to acquire read lock on auth");
-                return Ok(Ack {
-                    received: false,
-                    message: "Falha ao adquirir lock de leitura em auth".into(),
-                });
-            }
-        };
-    
+
+        let auth = self.auth.read().await;
+        
+        
         let is_valid = match auth.verify(bytes, &proposal.signature) {
             Ok(valid) => valid,
             Err(e) => {
@@ -64,15 +93,8 @@ impl Cluster {
         };
     
         if is_valid {
-            if let Ok(mut env) = self.local_env.write() {
-                env.engine.add_proposal(proposal.clone());
-            } else {
-                println!("⚠️ Failed to acquire write lock on local_env");
-                return Ok(Ack {
-                    received: false,
-                    message: "Falha ao adquirir lock de escrita em local_env".into(),
-                });
-            }
+            self.local_env.engine.lock().await
+                .add_proposal(proposal.clone());
     
             Ok(Ack {
                 received: true,
@@ -85,6 +107,12 @@ impl Cluster {
                 message: format!("Assinatura da proposta {} inválida", proposal.id),
             })
         }
+    }
+
+    pub(super) async fn evaluate_proposals(&self) -> Result<(), String> {
+        println!("🗳️ Avaliando consenso");
+        self.local_env.engine.lock().await.evaluate_proposals().await;
+        Ok(())
     }
     
 }
