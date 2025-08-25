df0c964347a625d36c27c48800b2a6f1c14fd5e4
Bruno <bruno@awdd.com.br>
2025-08-25 14:04:48 -0300

    refactor: apply Mutex to shutdown_sender and switch RwLock to tokio::RwLock

--- src/cluster/core.rs
+++ src/cluster/core.rs
@@ -1,6 +1,6 @@
 use std::{net::SocketAddr, sync::Arc};
 
-use tokio::sync::oneshot;
+use tokio::sync::{oneshot, Mutex, RwLock};
 
 use crate::{
     auth::Authenticator, 
@@ -27,7 +27,7 @@ pub struct Cluster {
     pub network: Arc<dyn NetworkAdapter>,
     pub local_node: Node,
     pub peer_manager: Arc<RwLock<PeerManager>>,
-    pub shutdown_sender: Option<oneshot::Sender<()>>,
+    pub shutdown_sender: Mutex<Option<oneshot::Sender<()>>>,
     pub auth: Arc<RwLock<dyn Authenticator>>,
 }
 
@@ -48,7 +48,7 @@ impl Cluster {
             network,
             local_node: Self::set_local_node(node_id, &addr),
             peer_manager,
-            shutdown_sender: None,
+            shutdown_sender: Mutex::new(None),
             auth
         }
     }
