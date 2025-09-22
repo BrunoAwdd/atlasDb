6b72246a5805f3e87b8e1bdb8579a7a3b0308886
Bruno <bruno@awdd.com.br>
2025-09-22 18:07:26 -0300

    🔥 chore: remove network from Cluster

--- src/cluster/core.rs
+++ src/cluster/core.rs
@@ -1,17 +1,18 @@
 use std::{net::SocketAddr, sync::Arc};
 
-use tokio::sync::{oneshot, Mutex, RwLock};
+use tokio::sync::{mpsc, oneshot, Mutex, RwLock};
 
 use crate::{
     auth::Authenticator, 
     config::Config, 
     env::AtlasEnv, 
-    network::adapter::NetworkAdapter,
+    network::p2p::adapter::AdapterCmd,
     peer_manager::PeerManager, 
     utils::NodeId, 
     Graph, 
     Storage
 };
+use crate::network::p2p::events::AdapterEvent;
 use super::node::Node;
 
 
@@ -27,7 +28,6 @@ use super::node::Node;
 pub struct Cluster {
     /// The full set of nodes currently part of the cluster.
     pub local_env: AtlasEnv,
-    pub network: Arc<dyn NetworkAdapter>,
     pub local_node: Node,
     pub peer_manager: Arc<RwLock<PeerManager>>,
     pub shutdown_sender: Mutex<Option<oneshot::Sender<()>>>,
@@ -38,26 +38,24 @@ impl Cluster {
     /// Initializes a new, empty cluster.
     pub fn new(
         env: AtlasEnv, 
-        network: Arc<dyn NetworkAdapter>,
         node_id: NodeId,
         auth: Arc<RwLock<dyn Authenticator>>,
     ) -> Self {
-        let addr = network.get_address();
+        let addr = "0.0.0.0:50052".to_string(); // Todo temp fix
 
         let peer_manager = Arc::clone(&env.peer_manager);
         
         Cluster {
             local_env: env,
-            network,
             local_node: Self::set_local_node(node_id, &addr),
             peer_manager,
             shutdown_sender: Mutex::new(None),
-            auth
+            auth,
         }
     }
 
     fn set_local_node(id: NodeId, addr: &str) -> Node {
-        Node::new(id.into(), addr.to_string(), None, 0.0)
+        Node::new(id, addr.to_string(), None, 0.0)
     }
 
     // @TODO: Is here the best place to save the state?
