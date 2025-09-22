ec850458b9bbede8720e04fe83901beef8b62c77
Bruno <bruno@awdd.com.br>
2025-09-22 18:14:00 -0300

    ➖ chore: remove gRPC proto dependency from Proposal

--- src/env/proposal.rs
+++ src/env/proposal.rs
@@ -1,6 +1,6 @@
 
 use serde::{Serialize, Deserialize};
-use crate::{cluster_proto::ProposalMessage, utils::NodeId};
+use crate::utils::NodeId;
 
 /// A proposal to mutate or modify shared graph state.
 ///
@@ -24,31 +24,6 @@ pub struct Proposal {
 }
     
 impl Proposal {
-    pub fn from_proto(msg: ProposalMessage) -> Result<Self, String> {
-        let signature: [u8; 64] = msg.signature.try_into()
-            .map_err(|_| "Assinatura inválida: deve ter 64 bytes".to_string())?;
-
-        Ok(Proposal {
-            id: msg.id,
-            proposer: NodeId(msg.proposer_id),
-            content: msg.content,
-            parent: if msg.parent_id.is_empty() { None } else { Some(msg.parent_id) },
-            signature,
-            public_key: msg.public_key.to_vec(),
-        })
-    }
-
-    pub fn into_proto(&self) -> ProposalMessage {
-        ProposalMessage {
-            id: self.id.clone(),
-            proposer_id: self.proposer.0.clone(),
-            content: self.content.clone(),
-            parent_id: self.parent.clone().unwrap_or_default(),
-            signature: self.signature.clone().into(),
-            public_key: self.public_key.clone().into(),
-        }
-    }
-
     pub fn from_json(json: &str) -> serde_json::Result<Self> {
         serde_json::from_str(json)
     }
